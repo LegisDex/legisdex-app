c0b7fad32a6a892aa36b10aa3aeb9fc45e4f8dd1
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-04-29 12:53:30 +0530

    Configure project metadata and Electron Forge build settings

--- forge.config.ts
+++ forge.config.ts
@@ -10,10 +10,14 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    icon: './public/favicon',
+    executableName: 'LegisDex',
   },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({}),
+    new MakerSquirrel({
+      setupIcon: './public/favicon.ico',
+    }),
     new MakerZIP({}, ['darwin']),
     new MakerRpm({}),
     new MakerDeb({}),
